85e2e887ddddf91a60aa72a3c1e47fb17208bbcc
lon8 <vlad.kanatik@ya.ru>
2023-03-27 18:47:15 +0300

    ya ebal pitonistov

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import openpyxl
 import xlsxwriter
-# from selenium import webdriver
+from selenium import webdriver
 
+MINROW = 11
+MAXROW = 1802
 
 workbook = openpyxl.load_workbook('result.xlsx')
 
@@ -12,40 +14,62 @@ worksheet = workbook.active
 wb = xlsxwriter.Workbook('test.xlsx')
 ws = wb.add_worksheet()
 
-value = worksheet.cell(row=11, column=2).value
-
-# browser = webdriver.Chrome('chromedriver.exe')
-
-# browser.get(f'https://exist.ru/Price/?pcode={value}')
-
-with open('index.html', 'r', encoding='utf-8') as file:
-    src = file.read()
-
-soup = BeautifulSoup(src, 'lxml')
-
-brand = soup.find('div', class_='art').text
-
-print(brand)
-
-offers = soup.find('div', class_='allOffers').find_all('div', class_='pricerow')
-
 counter = 0
 
-for offer in offers:
-    avail_bool = True
-    avail = offer.find('div', class_='avail').find('a', class_='gal')
-    if avail is None:
-        avail_bool = False
-    delivery_date = offer.find('span', class_='statis').text
-    price = offer.find('span', class_='price').text
-    price = ''.join([x for x in price if x.isdigit()])
-    print(price)
-    ws.write(counter, 0, brand)
-    ws.write(counter, 1, value ) # Артикул
-    ws.write(counter, 2, avail_bool)
-    ws.write(counter, 3, delivery_date)
-    ws.write(counter, 4, price)
-    counter += 1
+def kernel(href :str, brand_xlsx :str):
+    browser = webdriver.Chrome('chromedriver.exe')
 
-workbook.close()
-wb.close()
+    browser.get(href)
+
+    src = browser.page_source
+
+    soup = BeautifulSoup(src, 'lxml')
+
+    one_more_articles = soup.find('div', id='cat-wrapper')
+
+    if one_more_articles is not None:
+        lis = one_more_articles.find_all('li')
+        for li in lis:
+            brand_b = li.find('b').text
+            if brand_b.replace(' ', '') == brand_value.replace(' ', ''):
+                link = 'https://www.exist.ru/' + li.find('a')['href']
+                kernel(link, brand_xlsx)
+                return
+
+    brand = soup.find('div', class_='art').text
+
+    print(brand)
+
+    offers = soup.find('div', class_='allOffers').find_all('div', class_='pricerow')
+
+    
+
+    for offer in offers:
+        avail_bool = True
+        kernel(href, brand_xlsx)
+        try:
+            avail = offer.find('div', class_='avail').find('a', class_='gal')
+        except:
+            avail_bool = False
+        delivery_date = offer.find('span', class_='statis').text
+        price = offer.find('span', class_='price').text
+        price = ''.join([x for x in price if x.isdigit()])
+        print(price)
+        ws.write(counter, 0, brand)
+        ws.write(counter, 1, article_value ) # Артикул
+        ws.write(counter, 2, avail_bool)
+        ws.write(counter, 3, delivery_date)
+        ws.write(counter, 4, price)
+        counter += 1
+
+    workbook.close()
+    wb.close()
+
+for it in range(MINROW, MAXROW + 1):
+    article_value = worksheet.cell(row=it, column=2).value
+    brand_value = worksheet.cell(row=it, column=1).value
+    print(article_value)
+
+    href = f'https://exist.ru/Price/?pcode={article_value}'
+    kernel(href, brand_value)
+    print('Success --> ', href)
